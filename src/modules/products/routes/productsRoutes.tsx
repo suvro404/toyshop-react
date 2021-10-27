8707ee4205645a61831d4f1aa546b9ee173c8caf
suvro101 <debnathsuvro@gmail.com>
2021-10-27 23:37:23 +0600

    added redirect to base route

--- src/modules/products/routes/productsRoutes.tsx
+++ src/modules/products/routes/productsRoutes.tsx
@@ -4,8 +4,8 @@ import Product from "../views/Product";
 import {Route} from "react-router-dom";
 
 export const productsRoutes = (
-  <div>
+  <Route>
     <Route path='/products/:type' component={ProductList}></Route>
     <Route path='/product/:id' component={Product}></Route>
-  </div>
+  </Route>
 )
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,10 +3,13 @@ import { cartRoutes } from "../modules/cart/routes/cartRoutes";
 import { authRoutes } from "../modules/auth/routes/authRoutes";
 import { productsRoutes } from "../modules/products/routes/productsRoutes";
 
-import {Route} from "react-router-dom";
+import {Route, Redirect} from "react-router-dom";
 
 export const appRoutes = (
-  <Route path='' component={App}>
+  <Route>
+    <Route exact path="/">
+      <Redirect to="/products/trending" />
+    </Route>
     {productsRoutes}
     {cartRoutes}
     {authRoutes}
